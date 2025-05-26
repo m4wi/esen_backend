18f0738d10ee0b37b1a56a0005f1a467d50e1166
m4wi <mamanisandro951@gmail.com>
2025-05-25 21:59:40 -0500

    add 2 routes to users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 // TODO: add interface representing a user entity 
 export type User = any;
@@ -6,20 +7,83 @@ export type User = any;
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
+  constructor(
+    private prismaService: PrismaService
+  ){}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
+  async buscarUsuario ( userCode: string ) {
+    if ( !userCode ) {
+      throw new Error("usercode is null");
+    }
+    try {
+      const user = await this.prismaService.usuario.findUniqueOrThrow({
+        where: {
+          codigo_usuario: userCode
+        },
+        select: {
+          nombre: true,
+          apellido: true,
+          drive_folder: true,
+          usuario_id: true,
+          observacionesRecibidas: {
+            select: {
+              id_observacion: true,
+              fk_emisor: true,
+              fk_receptor: true,
+              contenido: true,
+              fecha: true,
+              descripcion: true
+            }
+          },
+          documentos: {
+            select: {
+              id_udoc: true,
+              drive_link: true
+            }
+          }
+        },
+      });
+
+      return user;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async listarDocumentosUsuario (userCode: string) {
+    if ( !userCode ) {
+      throw new Error("usercode is null");
+    }
+    try {
+      const userDocuments = await this.prismaService.usuario.findFirstOrThrow({
+        where:{
+          codigo_usuario: userCode
+        },
+        select: {
+          documentos: {
+            select: {
+              estado: true,
+              drive_link: true,
+              id_udoc: true,
+              documento: {
+                select: {
+                  id_documento: true,
+                  descripcion_requisito: true,
+                  subdescripciones: {
+                    select: {
+                      order: true,
+                      nombre_subdescripcion : true
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      });
+      return userDocuments;
+    } catch (error) {
+      throw new Error(error.message)
+    }
   }
 }
